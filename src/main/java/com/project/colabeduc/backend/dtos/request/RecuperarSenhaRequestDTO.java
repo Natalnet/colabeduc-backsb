510088fcaced13123b7cfc93057ba9266e10871c
Johnata Matheus <matheusabdala12@gmail.com>
2024-04-28 15:20:04 -0300

    feat: criar Dtos de recuperação e reset de senha

---
+++ src/main/java/com/project/colabeduc/backend/dtos/request/RecuperarSenhaRequestDTO.java
@@ -0,0 +1,3 @@
+package com.project.colabeduc.backend.dtos.request;
+
+public record RecuperarSenhaRequestDTO(String username) {}
---
+++ src/main/java/com/project/colabeduc/backend/dtos/request/ResetarSenhaRequestDTO.java
@@ -0,0 +1,3 @@
+package com.project.colabeduc.backend.dtos.request;
+
+public record ResetarSenhaRequestDTO(String newPassword) {}
